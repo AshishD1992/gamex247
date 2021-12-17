af6bc2155779a6d3e1fba622b8b3a2a822c0ff38
priti401 <pitimaurya401@gamil.com>
2021-12-17 20:04:40 +0530

    nav, midnav, rightnav changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { RightsideComponent } from './rightside/rightside.component';
+import { MidSideComponent } from './mid-side/mid-side.component';
 const routes: Routes = [
   {path:'', component:HeaderComponent,},
-
+{path:'nav', component:NavbarComponent,},
+{path:'right', component:RightsideComponent,},
+{path:'mid', component:MidSideComponent,},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RightsideComponent } from './rightside/rightside.component';
+
+import { MidSideComponent } from './mid-side/mid-side.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    NavbarComponent,
+    RightsideComponent,
+    
+    MidSideComponent,
   ],
   imports: [
     BrowserModule,
